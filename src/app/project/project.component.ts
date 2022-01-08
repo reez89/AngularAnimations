dce910776c8defb6518e5bb067427a7a4b0351c7
Nicola <n.rizzi@be-tse.it>
2022-01-08 16:05:39 +0100

    styled some components and small adjustament

---
+++ src/app/project/project.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Project } from '../projects/project.model';
+
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent implements OnInit {
+  @Input() project!: Project;
+  @Output() statusUpdated = new EventEmitter<string>();
+  @Output() projectDeleted = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onUpdateStatus(newStatus: string) {
+    this.statusUpdated.emit(newStatus);
+  }
+
+  onDelete() {
+    this.projectDeleted.emit();
+  }
+
+  getPrjstatusClass(){
+    return {
+      'bg-success': this.project.status === 'active',
+      'bg-secondary': this.project.status === 'inactive',
+      'bg-warning': this.project.status === 'critical'
+    }
+  }
+
+}
